7e3c16e51448301d9803d7d8b024de90a2933a9e
Victoria <egorova-victory16210@yandex.ru>
2022-11-22 22:18:06 +0400

    task15

---
+++ Seminar_2_task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введи цифру дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Day (int num) {
+  if(num==6||num==7){
+     Console.WriteLine("Это выходной день ");
+  }
+else if(num<1 || num>7){
+  Console.WriteLine("Это не день недели ");
+}
+else Console.WriteLine("Это не выходной день ");
+}
+Day(num);
